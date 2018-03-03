e4eaaae832e9283066d60b2ca59cc403fbd0b904
Rob Hinst <rob@hinst.net>
2018-03-03 21:46:45 +0000

    Updated stuff.

--- setup_insurgency.sh
+++ setup_insurgency.sh
@@ -3,8 +3,9 @@
 set -e
 
 GAMEDIR="/games"
-EBS_VOLUME_SIZE=20
+EBS_VOLUME_SIZE=16
 USERACCT=ubuntu
+INSTALLDIR="/home/ubuntu"
 
 echo "Updating package list..."
 apt-get -y update
@@ -13,10 +14,10 @@ echo "Installing required packages..."
 apt-get -y install lib32z1 lib32ncurses5 screen vim expect
 
 echo "Installing more required packages..."
-apt-get -y install lib32stdc++ lib32gcc1
+apt-get -y install lib32stdc++6 lib32gcc1
 
 echo "Creating $GAMEDIR directory..."
-mkdir "$GAMEDIR"
+mkdir -p "$GAMEDIR"
 
 echo "Mounting EBS volume..."
 DISKDEV=$(lsblk|grep 20G|cut -f1 -d" ")
@@ -33,24 +34,42 @@ fi
 echo "Updating permissions on $GAMEDIR..."
 chown $USERACCT /games
 
-sudo su - $USERACCT
-
-echo "Downloading Steam for Linux archive into /tmp..."
-cd /tmp
+echo "Downloading Steam for Linux archive into $INSTALLDIR..>"
+cd "$INSTALLDIR"
 /usr/bin/wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz
-
 echo "Extracting Steam installation files into $GAMEDIR..."
-cd "$GAMEDIR"
-/bin/tar xvfz steamcmd_linux.tar.gz
+/bin/tar xvfz /tmp/steamcmd_linux.tar.gz
 echo "Updating Steam installer permissions..."
+chown $USERACCT steamcmd.sh
 chmod +x steamcmd.sh
+
+echo "Updating Steam file..."
+su - $USERACCT -c "$INSTALLDIR/steamcmd.sh +runscript steam_update.txt"
 
 IP=$(ip add show eth0| grep "inet " | sed 's/.*inet \([[:digit:]]\+\)\.\([[:digit:]]\+\)\.\([[:digit:]]\+\)\.\([[:digit:]]\+\)\/.*/\1.\2\.\3.\4/')
 echo "Public IP address is $IP"
 
 echo "Generating game launch script..."
+cd "$GAMEDIR"
 echo "export LD_LIBRARY_PATH=/games/insurgency/bin:${LD_LIBRARY_PATH}" > start.sh
 echo "cd $GAMEDIR/insurgency" >> start.sh
 echo "./srcds_linux -console +map ministry +maxplayers 24 -ip $IP -port 27015" >> start.sh
+chown $USERACCT start.sh
 chmod +x start.sh
 
+echo "Generating game installer script..."
+cd "$GAMEDIR"
+echo "login" > install.sh
+echo "force_install_dir $GAMEDIR/" >> install.sh
+echo "app_update 740 validate" >> install.sh
+echo "quit" >> install.sh
+
+
+echo "NEXT STEPS: Run the following command as the $USERACCT user:"
+echo
+echo "$INSTALLDIR/steamcmd.sh +runscript $GAMEDIR/install.sh"
+echo
+echo "THEN: To start the server, run this command as the $USERACCT user:"
+echo
+echo "$GAMEDIR/start.sh"
+echo
